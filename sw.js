d73f7770f9863b20e485b5a33329b3c1eb992f5a
balajiventures-coder <balajiventuresdhule@gmail.com>
2026-04-22 15:55:35 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Minimal service worker to enable PWA "Add to Home Screen"
+const CACHE_NAME = 'godown-manager-v1';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
